caa7e1de23cd1f1b93515e03c7ca8333e799a293
Ranganath Kumar <reachmetwo@gmail.com>
2023-07-28 21:43:16 +0530

    Initial commit

---
+++ functionex2.py
@@ -0,0 +1,11 @@
+def greeting(name):
+   return name
+
+def main():
+    name1=input("\n Enter your First Name:")
+    greeting(name1)
+
+    name2=input("\n Enter your LastName:")
+    greeting(name1)
+
+main()
---
+++ jsonex1.py
@@ -0,0 +1,6 @@
+import requests
+
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json()
+
+print(json)
